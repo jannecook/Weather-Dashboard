70bf085b4275eebaa48c0c2f650786550e772028
jannecook <135638400+jannecook@users.noreply.github.com>
2023-12-10 20:11:54 -0800

    javascript update to include weather API URL & key

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+get weatherAPI = function (weather) {
+    var weatherApiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={e3e7f7f64d4b4b1ffd43dd04ba9ec514}';
+    fetch(weatherApiURL).then(function (response) {
+        
+    })
+
+get
+
+async
